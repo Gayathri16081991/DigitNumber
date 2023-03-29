da81ba69ddd5b4068641bc67e056c1886d1145e3
gayuvicky1615@gmail.com <gayuvicky1615@gmail.com>
2023-03-29 21:45:53 +0200

    Final Commit

--- src/test/java/org/example/stepdefinitions/DigitCountStepDefinition.java
+++ src/test/java/org/example/stepdefinitions/DigitCountStepDefinition.java
@@ -4,9 +4,11 @@ import static org.junit.Assert.assertEquals;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.en.And;
 
 public class DigitCountStepDefinition {
     private Integer number;
+    private Float numbers;
     private int digitCount;
 
     @Given("a java number parameter {int}")
@@ -23,4 +25,24 @@ public class DigitCountStepDefinition {
     public void result_should_be(Integer expectedDigitCount) {
         assertEquals((int) expectedDigitCount, digitCount);
     }
+
+    @And("a java float number parameter {float} {int}")
+    public void aJavaFloatNumberParameter(float numbers, int expectedDigitCount) {
+        this.numbers=numbers;
+        String numberString = Float.toString(numbers);
+
+        // Remove any leading minus sign
+        if (numberString.charAt(0) == '-') {
+            numberString = numberString.substring(1);
+        }
+
+        // Remove any decimal point and leading zeroes
+        numberString = numberString.replace(".", "").replaceFirst("^0+(?!$)", "");
+
+        // Count the remaining digits
+        digitCount = numberString.length();
+
+        assertEquals(expectedDigitCount, digitCount);
+    }
+
 }
--- src/test/resources/features/DigitCount.feature
+++ src/test/resources/features/DigitCount.feature
@@ -6,6 +6,16 @@ Feature: Count the number of digits in the integer part of a number
         Then result should be <expectedDigitCount>
 
     Examples:
-     | number | expectedDigitCount |
-     | 100    | 3 |
+      | number | expectedDigitCount |
+      | 100    | 3                  |
+      | -3214  | 4                  |
+      | 0000   | 1                  |
 
+  Scenario Outline: 02 - decimal case
+    And a java float number parameter <number> and <expectedDigitCount>
+
+    Examples:
+      | number   | expectedDigitCount |
+      | 100.546f | 3                  |
+      | -3214.12f | 4                  |
+      | 0001.35f  | 1                  |
